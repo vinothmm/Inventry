d73b663324f3b587c44c37d569899f5b24fc6bd9
vinothmm <52951069+vinothmm@users.noreply.github.com>
2020-05-16 11:38:24 +0800

    Add files via upload
    sql file

---
+++ DB.sql
@@ -0,0 +1,16 @@
+create table inventry (SNO int,
+							  BRAND varchar(100),
+							  MODEL varchar(100),
+							  PURCHASEDATE varchar(100),
+							  STATUS varchar(100));
+							  
+insert into inventry (sno,brand,purchasedate,model,status)
+values(1,'SAMSUNG','2011-05-12','TV','UNUSED');
+insert into inventry (sno,brand,purchasedate,model,status)
+values(2,'LG','2019-12-14','WM','NEW');
+insert into inventry (sno,brand,purchasedate,model,status)
+values(3,'SONY','2011-04-04','DVDPLAYER','OLD');
+insert into inventry (sno,brand,purchasedate,model,status)
+values(4,'NOKIA','2010-01-00','MOBILE','UNUSED');
+insert into inventry (sno,brand,purchasedate,model,status)
+values(5,'PHILIPS','2019-05-05','DRIYER','DPOILED');
